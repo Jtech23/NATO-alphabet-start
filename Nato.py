82a150e4839a5b1164045c74bd05124970e97b21
Jay_tech23 <jkolade746@gmail.com>
2024-02-09 21:33:06 -0800

    Update Nato.py

--- Nato.py
+++ Nato.py
@@ -8,6 +8,7 @@ dictionary = {row.letter: row.code for (index, row) in file.iterrows()}
 
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+
 word = input("Which word do you want to write in Nato Language: ").upper()
 nato = []
 for letter in word:
