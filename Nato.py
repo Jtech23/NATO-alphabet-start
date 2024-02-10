a57cfcd0c90206ab09876f2eb1a27078733d324d
JERRY <jkolade746@gmail.com>
2024-02-09 21:26:07 -0800

    README file

---
+++ Nato.py
@@ -0,0 +1,16 @@
+import pandas
+
+#TODO 1. Create a dictionary in this format:
+
+# {"A": "Alfa", "B": "Bravo"}
+file = pandas.read_csv("nato_phonetic_alphabet.csv")
+dictionary = {row.letter: row.code for (index, row) in file.iterrows()}
+
+
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+word = input("Which word do you want to write in Nato Language: ").upper()
+nato = []
+for letter in word:
+    nato += ([value for (key, value) in (dictionary.items()) if key == letter])
+print(nato)
+
